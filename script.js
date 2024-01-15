71a6ff5bd42bbc687dca87266f741e8e0b5135a1
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-01-15 01:20:35 +0100

    update

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ searchBtn.addEventListener("click", () => {
     fetch(finalURL)
         .then((response) => response.json())
         .then((data) => {
-            //   console.log(data[0]);
-            //   console.log(data[0].capital[0]);
-            //   console.log(data[0].flags.svg);
-            //   console.log(data[0].name.common);
-            //   console.log(data[0].continents[0]);
-            //   console.log(Object.keys(data[0].currencies)[0]);
-            //   console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-            //   console.log(
-            //     Object.values(data[0].languages).toString().split(",").join(", ")
-            //   );
+              console.log(data[0]);
+              console.log(data[0].capital[0]);
+              console.log(data[0].flags.svg);
+              console.log(data[0].name.common);
+              console.log(data[0].continents[0]);
+              console.log(Object.keys(data[0].currencies)[0]);
+              console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+              console.log(
+                Object.values(data[0].languages).toString().split(",").join(", ")
+              );
             result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
         <h2>${data[0].name.common}</h2>
